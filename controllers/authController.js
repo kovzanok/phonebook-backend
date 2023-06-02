8a9a1e33895fe5a39afb9fe9e75d453309f9a89a
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-02 21:10:06 +0300

    feat: add authController methods

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,57 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+const genrateAccessToken = (id) => {
+  const payload = { id };
+  return jwt.sign(payload, "secret123", { expiresIn: "24h" });
+};
+
 class authController {
   async registration(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res
+          .status(400)
+          .json({ message: "Ошибка при регистрации", errors });
+      }
+      const { login, password } = req.body;
+      const candidate = await User.findOne({ login });
+      if (candidate) {
+        return res.status(400).json({ message: "Ошибка регистрации" });
+      }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const user = new User({ login, password: hashPassword, clients: [] });
+      await user.save();
+      return res.json({ message: "Регистрация прошла успешно" });
     } catch (e) {
       console.log(e.message);
+      res.status(400).json({ message: "Ошибка регистрации" });
     }
   }
 
   async login(req, res) {
     try {
+      const { login, password } = req.body;
+      console.log(login)
+      const user = await User.findOne({ login });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: `Пользователь ${login} не найден` });
+      }
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: `Неверный логин или пароль` });
+      }
+
+      const token = genrateAccessToken(user._id);
+      return res.json({token})
     } catch (e) {
       console.log(e.message);
+      res.status(400).json({ message: "Ошибка авторизации" });
     }
   }
 
