1c35e2414072e82363aa74d40c7f50f3d6b2198b
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-02 14:20:54 +0300

    feat: add client schema

---
+++ models/Client.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const Client = new Schema({
+  name: { type: String, required: true },
+  substaions: [{ name: { type: String, required: true }, info: String }],
+  people: [
+    {
+      name: { type: String, required: true },
+      position: { type: String, required: true },
+      phones: [String],
+      email: [String],
+      info: String,
+    },
+  ],
+  contacts: [
+    {
+      name: { type: String, required: true },
+      phones: [String],
+      email: [String],
+    },
+  ],
+});
+
+module.exports = model("Client", Client);
