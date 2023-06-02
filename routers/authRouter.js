0417911266979bb5a7ea316f865ab50227f7401a
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-02 21:10:49 +0300

    feat: add validation on post route

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,10 +1,20 @@
-const authController= require('../controllers/authController.js')
-const Router = require('express');
+const authController = require("../controllers/authController.js");
+const Router = require("express");
+const { check } = require("express-validator");
+
 const router = new Router();
 
+router.post(
+  "/registration",
+  [
+    check("login", "Введите логин").notEmpty(),
+    check("password", "Минимальная длина пароля 5 символов").isLength({
+      min: 5,
+    }),
+  ],
+  authController.registration
+);
+router.post("/login", authController.login);
+router.get("/users", authController.getUsers);
 
-router.post('/registration',authController.registration);
-router.post('/login',authController.login);
-router.get('/users',authController.getUsers);
-
-module.exports = router
+module.exports = router;
