b5904f39f613a4ef6100a397e0eadb0032fb8203
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-02 21:07:19 +0300

    feat: change clients schema property

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const User = new Schema({
   login: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  clients: [{type:String, ref:'Client'}]
+  clients: [{ type: Schema.Types.ObjectId, ref: "Client" }],
 });
 
-module.exports = model('User', User);
+module.exports = model("User", User);
