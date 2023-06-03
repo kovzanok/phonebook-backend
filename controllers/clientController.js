7848feeaabb76f8c67bcf171367b003be730ef79
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-03 09:59:50 +0300

    feat: add get clients method to controller

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,5 +1,12 @@
+const UserModel = require("../models/User.js");
+const ClientModel = require("../models/Client.js");
+
 class clientController {
-  async getClients(req, res) {}
+  async getClients(req, res) {
+    const id = req.userId;
+    const user = await UserModel.findById(id).populate("clients").exec();
+    res.json(user.clients);
+  }
 
   async getClient(req,res) {}
 
