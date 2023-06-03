bd0ab88af4a6ff80df34269a0fa455efd1fd9c94
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-03 13:15:51 +0300

    feat: add cors to express app

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require('mongoose');
 const authRouter = require("./routers/authRouter.js");
 const clientsRouter = require("./routers/clientsRouter.js");
+const cors = require("cors");
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/clients", clientsRouter);
