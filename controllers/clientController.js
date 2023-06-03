8df382aa5ffa8d5886947cf967f45110a3c66497
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-03 08:16:38 +0300

    feat: add client controller

---
+++ controllers/clientController.js
@@ -0,0 +1,13 @@
+class clientController {
+  async getClients(req, res) {}
+
+  async getClient(req,res) {}
+
+  async createClient(req, res) {}
+
+  async updateClient(req, res) {}
+
+  async deleteClient(req,res) {}
+}
+
+module.exports = new clientController();
